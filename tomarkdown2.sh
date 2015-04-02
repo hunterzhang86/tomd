e8fed205e674729165eee9af9fe41d37b73713c9
hunterzhang86 <hunterzhang86@gmail.com>
2015-04-02 11:55:54 +0800

    正文提取

--- tomarkdown2.sh
+++ tomarkdown2.sh
@@ -1,6 +1,6 @@
 # $1、$2分别是存放HTML文件的文件夹 和 你要存放的笔记分支 ， 笔记分支比如 test
 
-sudo python dumphtmlandimages.py $1 $2
+sudo python htm_body_extractor.py $1 $2
 
 for filename in `ls $2`
 do
@@ -10,9 +10,9 @@ do
 		fname=`basename $filename .html`
 		name=${fname%.html}
 		echo $name
-		python  html2text.py $2$name.html utf8 > $2$name.md
+		python html2text.py $2$name.html utf8 > $2$name.md
 	fi
-	rm $2$name.html
+	# rm $2$name.html
 done
 
 echo "Success !"
